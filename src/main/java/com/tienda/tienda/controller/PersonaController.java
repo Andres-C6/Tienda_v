d5d133b5d5c599685c167fa508eca108e5ff798e
ancar <ancar@AndresPC>
2022-04-11 01:30:42 -0600

    PersonaController

--- src/main/java/com/tienda/tienda/controller/PersonaController.java
+++ src/main/java/com/tienda/tienda/controller/PersonaController.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class PersonaController {
@@ -14,11 +17,32 @@ public class PersonaController {
     @Autowired
     private IPersonaService personaService;
 
-    @GetMapping("/personas")
+    @GetMapping("/persona")
     public String index(Model model) {
         List<Persona> listaPersonas = personaService.getAllPerson();
         model.addAttribute("titulo", "Personas");
         model.addAttribute("personas", listaPersonas);
         return "personas";
     }
+    
+    
+    @GetMapping("/personasN")
+    public String crearPersona(Model model) {
+        model.addAttribute("personas",new Persona());
+        //persona.Service.savePerson(persona);
+        return "crear";
+    }
+    
+    @PostMapping("/save")
+    public String guardarPersona(@ModelAttribute Persona persona) {
+        personaService.savePerson(persona);
+        return "redirect:/Persona";
+    }
+    
+    @GetMapping("/delete/{id}")
+    public String eliminarPersona(@PathVariable("id")Long idPersona) {
+        personaService.delete(idPersona);
+        return "crear";
+    }
+    
 }
